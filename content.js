e2f8e4938d75cc1e96f04129fd0c624c9441a9f9
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2024-06-27 02:38:53 +0900

    init, loaded

--- content.js
+++ content.js
@@ -4,21 +4,10 @@ import(chrome.runtime.getURL('common.js')).then(common => {
 });
 
 function main(common) {
-    new MutationObserver((mutations, observer) => {
-        if (app.querySelector('div.ytp-right-controls')) {
-            observer.disconnect();
-            apply_settings();
-        }
-    }).observe(app, { childList: true, subtree: true });
-
-    chrome.storage.onChanged.addListener(() => {
-        apply_settings();
-    });
-
-    function apply_settings() {
+    function loadSettings() {
         chrome.storage.local.get(common.storage, data => {
             create_buttons(data);
-            document.dispatchEvent(new CustomEvent('_tap_rate_init'));
+            document.dispatchEvent(new CustomEvent('_tap_rate_loaded'));
         });
     }
 
@@ -49,6 +38,20 @@ function main(common) {
         });
         area.insertBefore(button, panel);
     }
+
+    document.addEventListener('_tap_volume_init', e => {
+        new MutationObserver((mutations, observer) => {
+            if (app.querySelector('span.ytp-volume-area')) {
+                observer.disconnect();
+                loadSettings();
+            }
+        }).observe(app, { childList: true, subtree: true });
+        loadSettings();
+    });
+
+    chrome.storage.onChanged.addListener(() => {
+        loadSettings();
+    });
 
     const s = document.createElement('script');
     s.src = chrome.runtime.getURL('inject.js');
--- inject.js
+++ inject.js
@@ -1,43 +1,44 @@
-const _tap_rate_app = document.querySelector('ytd-app');
-if (_tap_rate_app) {
-    function _tap_rate_update_class(remove_class, add_target_class, add_value) {
-        for (const button of _tap_rate_app.querySelectorAll('button._tap_rate_button')) {
-            const oldToken = button.classList.contains(remove_class) ? remove_class : undefined;
-            const newToken = button.classList.contains(add_target_class) ? add_value : undefined;
-            if (oldToken && newToken) {
-                button.classList.replace(oldToken, newToken);
-            } else if (oldToken) {
-                button.classList.remove(oldToken);
-            } else if (newToken) {
-                button.classList.add(newToken);
-            }
+const _tap_rate_app = document.querySelector('ytd-app') ?? document.body;
+
+function _tap_rate_update_class(remove_class, add_target_class, add_value) {
+    for (const button of _tap_rate_app.querySelectorAll('button._tap_rate_button')) {
+        const oldToken = button.classList.contains(remove_class) ? remove_class : undefined;
+        const newToken = button.classList.contains(add_target_class) ? add_value : undefined;
+        if (oldToken && newToken) {
+            button.classList.replace(oldToken, newToken);
+        } else if (oldToken) {
+            button.classList.remove(oldToken);
+        } else if (newToken) {
+            button.classList.add(newToken);
         }
     }
-
-    function _tap_rate_activate(value) {
-        _tap_rate_update_class('_tap_rate_active', '_tap_rate_button_' + value.toString().replace('.', '_'), '_tap_rate_active');
-    }
-
-    let _tap_rate_init = true;
-
-    document.addEventListener('_tap_rate_init', e => {
-        const player = _tap_rate_app.querySelector('div#movie_player');
-        if (player) {
-            _tap_rate_activate(player.getPlaybackRate());
-            if (_tap_rate_init) {
-                _tap_rate_init = false;
-                player.addEventListener('onPlaybackRateChange', e => {
-                    _tap_rate_activate(e);
-                });
-            }
-        }
-    });
-
-    document.addEventListener('_tap_rate', e => {
-        _tap_rate_update_class('_tap_rate_tap', '_tap_rate_button_' + e.detail.toString().replace('.', '_'), '_tap_rate_tap');
-        const player = _tap_rate_app.querySelector('div#movie_player');
-        if (player) {
-            player.setPlaybackRate(e.detail);
-        }
-    });
 }
+
+function _tap_rate_activate(value) {
+    _tap_rate_update_class('_tap_rate_active', '_tap_rate_button_' + value.toString().replace('.', '_'), '_tap_rate_active');
+}
+
+let _tap_rate_init = true;
+
+document.addEventListener('_tap_rate_loaded', e => {
+    const player = _tap_rate_app.querySelector('div#movie_player');
+    if (player) {
+        _tap_rate_activate(player.getPlaybackRate());
+        if (_tap_rate_init) {
+            _tap_rate_init = false;
+            player.addEventListener('onPlaybackRateChange', e => {
+                _tap_rate_activate(e);
+            });
+        }
+    }
+});
+
+document.addEventListener('_tap_rate', e => {
+    _tap_rate_update_class('_tap_rate_tap', '_tap_rate_button_' + e.detail.toString().replace('.', '_'), '_tap_rate_tap');
+    const player = _tap_rate_app.querySelector('div#movie_player');
+    if (player) {
+        player.setPlaybackRate(e.detail);
+    }
+});
+
+document.dispatchEvent(new CustomEvent('_tap_rate_init'));
