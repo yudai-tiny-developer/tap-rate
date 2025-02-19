cddc1258aac75ef6cb8f3b9cfaa1e485fe9e0892
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2025-02-19 20:47:52 +0900

    v1.8.1

--- content.js
+++ content.js
@@ -5,36 +5,35 @@ import(chrome.runtime.getURL('common.js')).then(common => {
 });
 
 function main(app, common) {
-    function applySettings(force) {
-        if (settings) {
-            update_buttons(settings, force);
-            update_shortcut_command(settings);
-            document.dispatchEvent(new CustomEvent('_tap_rate_loaded'));
-        } else {
-            loadSettings();
-        }
-    }
-
     function loadSettings() {
+        const area = app.querySelector('div.ytp-right-controls');
+        if (!area) {
+            return false;
+        }
+
+        const panel = area.querySelector('button.ytp-settings-button');
+        if (!panel) {
+            return false;
+        }
+
         chrome.storage.local.get(common.storage, data => {
-            settings = data;
-            applySettings(true);
+            update_buttons(data, area, panel);
+            update_shortcut_command(data);
+            document.dispatchEvent(new CustomEvent('_tap_rate_loaded'));
         });
+
+        return true;
     }
 
-    function update_buttons(data, force) {
-        const area = app.querySelector('div.ytp-right-controls');
-        if (area && (!area.querySelector('button._tap_rate_button') || force)) {
-            let panel = area.querySelector('button.ytp-settings-button');
-            panel = update_button(data.v7, common.default_v7, area, panel, data.v7_enabled, common.default_v7_enabled);
-            panel = update_button(data.v8, common.default_v8, area, panel, data.v8_enabled, common.default_v8_enabled);
-            panel = update_button(data.v6, common.default_v6, area, panel, data.v6_enabled, common.default_v6_enabled);
-            panel = update_button(data.v5, common.default_v5, area, panel, data.v5_enabled, common.default_v5_enabled);
-            panel = update_button(data.v4, common.default_v4, area, panel, data.v4_enabled, common.default_v4_enabled);
-            panel = update_button(data.v3, common.default_v3, area, panel, data.v3_enabled, common.default_v3_enabled);
-            panel = update_button(data.v2, common.default_v2, area, panel, data.v2_enabled, common.default_v2_enabled);
-            panel = update_button(data.v1, common.default_v1, area, panel, data.v1_enabled, common.default_v1_enabled);
-        }
+    function update_buttons(data, area, panel) {
+        panel = update_button(data.v7, common.default_v7, area, panel, data.v7_enabled, common.default_v7_enabled);
+        panel = update_button(data.v8, common.default_v8, area, panel, data.v8_enabled, common.default_v8_enabled);
+        panel = update_button(data.v6, common.default_v6, area, panel, data.v6_enabled, common.default_v6_enabled);
+        panel = update_button(data.v5, common.default_v5, area, panel, data.v5_enabled, common.default_v5_enabled);
+        panel = update_button(data.v4, common.default_v4, area, panel, data.v4_enabled, common.default_v4_enabled);
+        panel = update_button(data.v3, common.default_v3, area, panel, data.v3_enabled, common.default_v3_enabled);
+        panel = update_button(data.v2, common.default_v2, area, panel, data.v2_enabled, common.default_v2_enabled);
+        panel = update_button(data.v1, common.default_v1, area, panel, data.v1_enabled, common.default_v1_enabled);
     }
 
     function update_button(data, default_value, area, panel, enabled, default_enabled) {
@@ -47,8 +46,8 @@ function main(app, common) {
     function create_button(value, area, panel) {
         const button = document.createElement('button');
         button.style.display = 'none';
-        button.innerHTML = `<svg width="100%" height="100%" viewBox="0 0 72 72"><text font-size="20" x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">${value}x</text></svg>`;
         button.classList.add('_tap_rate_button', '_tap_rate_button_' + value.toString().replace('.', '_'), 'ytp-button');
+        button.innerHTML = `<svg width="100%" height="100%" viewBox="0 0 72 72"><text font-size="20" x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">${value}x</text></svg>`;
         button.addEventListener('click', () => {
             document.dispatchEvent(new CustomEvent('_tap_rate', { detail: value }));
         });
@@ -91,23 +90,20 @@ function main(app, common) {
         };
     }
 
-    let settings;
-    let observer;
     let shortcut_command;
-
-    document.addEventListener('_tap_rate_init', e => {
-        loadSettings();
-        observer?.disconnect();
-        observer = new MutationObserver(() => applySettings());
-        observer.observe(app, { childList: true, subtree: true });
-    });
-
-    chrome.storage.onChanged.addListener(() => {
-        loadSettings();
-    });
-
     chrome.runtime.onMessage.addListener(command => {
-        shortcut_command(command);
+        if (shortcut_command) {
+            shortcut_command(command);
+        }
+    });
+
+    chrome.storage.onChanged.addListener(loadSettings);
+    document.addEventListener('_tap_rate_init', e => {
+        const interval = setInterval(() => {
+            if (loadSettings()) {
+                clearInterval(interval);
+            }
+        }, 200);
     });
 
     const s = document.createElement('script');
--- inject.js
+++ inject.js
@@ -1,45 +1,50 @@
-const _tap_rate_app = document.querySelector('ytd-app') ?? document.body;
+(() => {
+    let player;
+    let area;
+    let onPlaybackRateChange;
 
-function _tap_rate_update_class(remove_class, add_target_class, add_value) {
-    for (const button of _tap_rate_app.querySelectorAll('button._tap_rate_button')) {
-        const oldToken = button.classList.contains(remove_class) ? remove_class : undefined;
-        const newToken = button.classList.contains(add_target_class) ? add_value : undefined;
-        if (oldToken && newToken) {
-            button.classList.replace(oldToken, newToken);
-        } else if (oldToken) {
-            button.classList.remove(oldToken);
-        } else if (newToken) {
-            button.classList.add(newToken);
+    function update_class(remove_class, add_target_class, add_value) {
+        area = area ?? document.querySelector('div.ytp-right-controls');
+        if (area) {
+            for (const button of area.querySelectorAll('button._tap_rate_button')) {
+                const oldToken = button.classList.contains(remove_class) ? remove_class : undefined;
+                const newToken = button.classList.contains(add_target_class) ? add_value : undefined;
+                if (oldToken && newToken) {
+                    button.classList.replace(oldToken, newToken);
+                } else if (oldToken) {
+                    button.classList.remove(oldToken);
+                } else if (newToken) {
+                    button.classList.add(newToken);
+                }
+            }
         }
     }
-}
 
-function _tap_rate_activate(value) {
-    _tap_rate_update_class('_tap_rate_active', '_tap_rate_button_' + value.toString().replace('.', '_'), '_tap_rate_active');
-}
+    function activate(value) {
+        update_class('_tap_rate_active', '_tap_rate_button_' + value.toString().replace('.', '_'), '_tap_rate_active');
+    }
 
-let _tap_rate_init = true;
+    document.addEventListener('_tap_rate_loaded', () => {
+        player = player ?? document.querySelector('div#movie_player');
+        if (player) {
+            activate(player.getPlaybackRate());
 
-document.addEventListener('_tap_rate_loaded', e => {
-    const player = _tap_rate_app.querySelector('div#movie_player');
-    if (player) {
-        _tap_rate_activate(player.getPlaybackRate());
-        if (_tap_rate_init) {
-            _tap_rate_init = false;
-            player.addEventListener('onPlaybackRateChange', e => {
-                _tap_rate_activate(e);
-            });
+            if (!onPlaybackRateChange) {
+                onPlaybackRateChange = e => {
+                    activate(e);
+                };
+                player.addEventListener('onPlaybackRateChange', onPlaybackRateChange);
+            }
         }
-    }
-});
+    });
 
-document.addEventListener('_tap_rate', e => {
-    _tap_rate_update_class('_tap_rate_tap', '_tap_rate_button_' + e.detail.toString().replace('.', '_'), '_tap_rate_tap');
-    const player = _tap_rate_app.querySelector('div#movie_player');
-    if (player) {
-        player.setPlaybackRate(e.detail);
-        setTimeout(() => player.dispatchEvent(new MouseEvent('mouseout')), 500);
-    }
-});
+    document.addEventListener('_tap_rate', e => {
+        player = player ?? document.querySelector('div#movie_player');
+        if (player) {
+            player.setPlaybackRate(e.detail);
+            setTimeout(() => player.dispatchEvent(new MouseEvent('mouseout')), 500);
+        }
+    });
 
-document.dispatchEvent(new CustomEvent('_tap_rate_init'));
+    document.dispatchEvent(new CustomEvent('_tap_rate_init'));
+})();
