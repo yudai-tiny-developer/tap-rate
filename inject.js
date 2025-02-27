b892b964dfe13bcb1168ca8b96de0e76b8d4862c
Simple and Tiny Utilities <108601122+yudai-tiny-developer@users.noreply.github.com>
2025-02-28 07:59:14 +0900

    V1.8.3 (#28)
    * v1.8.3
    
    * settings
    
    * inject
    
    * detect_interval
    
    * detect_interval
    
    ---------
    
    Co-authored-by: yudai-tiny-developer <yudai-tiny-developer@gmail.com>

--- inject.js
+++ inject.js
@@ -1,10 +1,5 @@
 (() => {
-    let player;
-    let area;
-    let onPlaybackRateChange;
-
     function update_class(remove_class, add_target_class, add_value) {
-        area = area ?? document.querySelector('div.ytp-right-controls');
         if (area) {
             for (const button of area.querySelectorAll('button._tap_rate_button')) {
                 const oldToken = button.classList.contains(remove_class) ? remove_class : undefined;
@@ -24,27 +19,37 @@
         update_class('_tap_rate_active', '_tap_rate_button_' + value.toString().replace('.', '_'), '_tap_rate_active');
     }
 
+    const app = document.querySelector('ytd-app') ?? document.body; // YouTube.com or Embedded Player
+
+    let player;
+    let area;
+
     document.addEventListener('_tap_rate_loaded', () => {
-        player = player ?? document.querySelector('div#movie_player');
         if (player) {
             activate(player.getPlaybackRate());
-
-            if (!onPlaybackRateChange) {
-                onPlaybackRateChange = e => {
-                    activate(e);
-                };
-                player.addEventListener('onPlaybackRateChange', onPlaybackRateChange);
-            }
         }
     });
 
     document.addEventListener('_tap_rate', e => {
-        player = player ?? document.querySelector('div#movie_player');
         if (player) {
             player.setPlaybackRate(e.detail);
             setTimeout(() => player.dispatchEvent(new MouseEvent('mouseout')), 500);
         }
     });
 
-    document.dispatchEvent(new CustomEvent('_tap_rate_init'));
+    const detect_interval = setInterval(() => {
+        player = app.querySelector('div#movie_player');
+        if (player) {
+            area = player.querySelector('div.ytp-right-controls');
+            if (area) {
+                clearInterval(detect_interval);
+
+                player.addEventListener('onPlaybackRateChange', e => {
+                    activate(e);
+                });
+
+                document.dispatchEvent(new CustomEvent('_tap_rate_init'));
+            }
+        }
+    }, 500);
 })();
