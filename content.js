a3f051054129db67f80beb9a9d5f4aeae8bf83df
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2025-02-28 02:33:34 +0900

    settings

--- content.js
+++ content.js
@@ -6,103 +6,119 @@ import(chrome.runtime.getURL('common.js')).then(common => {
 
 function main(app, common) {
     function loadSettings() {
-        const area = app.querySelector('div.ytp-right-controls');
-        if (!area) {
-            return false;
-        }
-
-        const panel = area.querySelector('button.ytp-settings-button');
-        if (!panel) {
-            return false;
-        }
-
         chrome.storage.local.get(common.storage, data => {
-            update_buttons(data, area, panel);
-            update_shortcut_command(data);
+            settings = data;
+            update_buttons();
             document.dispatchEvent(new CustomEvent('_tap_rate_loaded'));
         });
-
-        return true;
     }
 
-    function update_buttons(data, area, panel) {
-        panel = update_button(data.v7, common.default_v7, area, panel, data.v7_enabled, common.default_v7_enabled);
-        panel = update_button(data.v8, common.default_v8, area, panel, data.v8_enabled, common.default_v8_enabled);
-        panel = update_button(data.v6, common.default_v6, area, panel, data.v6_enabled, common.default_v6_enabled);
-        panel = update_button(data.v5, common.default_v5, area, panel, data.v5_enabled, common.default_v5_enabled);
-        panel = update_button(data.v4, common.default_v4, area, panel, data.v4_enabled, common.default_v4_enabled);
-        panel = update_button(data.v3, common.default_v3, area, panel, data.v3_enabled, common.default_v3_enabled);
-        panel = update_button(data.v2, common.default_v2, area, panel, data.v2_enabled, common.default_v2_enabled);
-        panel = update_button(data.v1, common.default_v1, area, panel, data.v1_enabled, common.default_v1_enabled);
+    function update_buttons() {
+        if (settings) {
+            update_button(button_v1, settings.v1, common.default_v1, settings.v1_enabled, common.default_v1_enabled);
+            update_button(button_v2, settings.v2, common.default_v2, settings.v2_enabled, common.default_v2_enabled);
+            update_button(button_v3, settings.v3, common.default_v3, settings.v3_enabled, common.default_v3_enabled);
+            update_button(button_v4, settings.v4, common.default_v4, settings.v4_enabled, common.default_v4_enabled);
+            update_button(button_v5, settings.v5, common.default_v5, settings.v5_enabled, common.default_v5_enabled);
+            update_button(button_v6, settings.v6, common.default_v6, settings.v6_enabled, common.default_v6_enabled);
+            update_button(button_v7, settings.v7, common.default_v7, settings.v7_enabled, common.default_v7_enabled);
+            update_button(button_v8, settings.v8, common.default_v8, settings.v8_enabled, common.default_v8_enabled);
+        }
     }
 
-    function update_button(data, default_value, area, panel, enabled, default_enabled) {
-        const value = common.value(data, default_value).toString().replace('.', '_');
-        const button = area.querySelector('button._tap_rate_button_' + value) ?? create_button(common.value(data, default_value), area, panel);
+    function update_button(button, value, default_value, enabled, default_enabled) {
+        const label = common.value(value, default_value);
+        const detail = label.toString().replace('.', '_');
         button.style.display = common.value(enabled, default_enabled) ? '' : 'none';
-        return button;
+        button.classList.add('_tap_rate_button', '_tap_rate_button_' + detail, 'ytp-button');
+        button.innerHTML = `<svg width="100%" height="100%" viewBox="0 0 72 72"><text font-size="20" x="50%" y="50%" dominant-baseline="central" text-anchor="middle">${label}x</text></svg>`;
+        button.addEventListener('click', () => {
+            document.dispatchEvent(new CustomEvent('_tap_rate', { detail: label }));
+        });
     }
 
-    function create_button(value, area, panel) {
+    function create_button() {
         const button = document.createElement('button');
         button.style.display = 'none';
-        button.classList.add('_tap_rate_button', '_tap_rate_button_' + value.toString().replace('.', '_'), 'ytp-button');
-        button.innerHTML = `<svg width="100%" height="100%" viewBox="0 0 72 72"><text font-size="20" x="50%" y="50%" dominant-baseline="central" text-anchor="middle">${value}x</text></svg>`;
-        button.addEventListener('click', () => {
-            document.dispatchEvent(new CustomEvent('_tap_rate', { detail: value }));
-        });
-        area.insertBefore(button, panel);
         return button;
     }
 
-    function update_shortcut_command(data) {
-        shortcut_command = command => {
+    const shortcut_command = command => {
+        if (settings) {
             let value;
             switch (command) {
                 case 'v1':
-                    value = common.value(data.v1, common.default_v1);
+                    value = common.value(settings.v1, common.default_v1);
                     break;
                 case 'v2':
-                    value = common.value(data.v2, common.default_v2);
+                    value = common.value(settings.v2, common.default_v2);
                     break;
                 case 'v3':
-                    value = common.value(data.v3, common.default_v3);
+                    value = common.value(settings.v3, common.default_v3);
                     break;
                 case 'v4':
-                    value = common.value(data.v4, common.default_v4);
+                    value = common.value(settings.v4, common.default_v4);
                     break;
                 case 'v5':
-                    value = common.value(data.v5, common.default_v5);
+                    value = common.value(settings.v5, common.default_v5);
                     break;
                 case 'v6':
-                    value = common.value(data.v6, common.default_v6);
+                    value = common.value(settings.v6, common.default_v6);
                     break;
                 case 'v7':
-                    value = common.value(data.v8, common.default_v8);
+                    value = common.value(settings.v8, common.default_v8);
                     break;
                 case 'v8':
-                    value = common.value(data.v7, common.default_v7);
+                    value = common.value(settings.v7, common.default_v7);
                     break;
                 default:
                     return;
             }
             document.dispatchEvent(new CustomEvent('_tap_rate', { detail: value }));
-        };
-    }
-
-    let shortcut_command;
-    chrome.runtime.onMessage.addListener(command => {
-        if (shortcut_command) {
-            shortcut_command(command);
         }
-    });
+    };
+
+    const button_v1 = create_button();
+    const button_v2 = create_button();
+    const button_v3 = create_button();
+    const button_v4 = create_button();
+    const button_v5 = create_button();
+    const button_v6 = create_button();
+    const button_v7 = create_button();
+    const button_v8 = create_button();
+
+    let settings;
+    let area;
+    let panel;
+
+    chrome.runtime.onMessage.addListener(shortcut_command);
 
     chrome.storage.onChanged.addListener(loadSettings);
-    document.addEventListener('_tap_rate_init', e => {
-        const interval = setInterval(() => {
-            if (loadSettings()) {
-                clearInterval(interval);
+
+    document.addEventListener('_tap_rate_init', () => {
+        const detect_interval = setInterval(() => {
+            area = app.querySelector('div.ytp-right-controls');
+            if (!area) {
+                return;
             }
+
+            panel = area.querySelector('button.ytp-settings-button');
+            if (!panel) {
+                return;
+            }
+
+            clearInterval(detect_interval);
+
+            area.insertBefore(button_v7, panel);
+            area.insertBefore(button_v8, button_v7);
+            area.insertBefore(button_v6, button_v8);
+            area.insertBefore(button_v5, button_v6);
+            area.insertBefore(button_v4, button_v5);
+            area.insertBefore(button_v3, button_v4);
+            area.insertBefore(button_v2, button_v3);
+            area.insertBefore(button_v1, button_v2);
+
+            loadSettings();
         }, 200);
     });
 
