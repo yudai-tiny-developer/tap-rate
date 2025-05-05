05b9eedef778c2431e530a5989cdc92709a34d2d
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2025-05-05 21:20:05 +0900

    v1.9.0

--- common.js
+++ common.js
@@ -7,6 +7,14 @@ export const storage = [
     'v6', 'v6_enabled',
     'v7', 'v7_enabled',
     'v8', 'v8_enabled',
+    'w1', 'w1_enabled',
+    'w2', 'w2_enabled',
+    'w3', 'w3_enabled',
+    'w4', 'w4_enabled',
+    'w5', 'w5_enabled',
+    'w6', 'w6_enabled',
+    'w7', 'w7_enabled',
+    'w8', 'w8_enabled',
 ];
 
 export const default_v1 = 0.25;
@@ -17,6 +25,14 @@ export const default_v5 = 1.25;
 export const default_v6 = 1.5;
 export const default_v7 = 2;
 export const default_v8 = 1.75;
+export const default_w1 = 3.0;
+export const default_w2 = 4.0;
+export const default_w3 = 5.0;
+export const default_w4 = 6.0;
+export const default_w5 = 8.0;
+export const default_w6 = 10.0;
+export const default_w7 = 12.0;
+export const default_w8 = 16.0;
 
 export const default_v1_enabled = false; // 0.25
 export const default_v2_enabled = false; // 0.5
@@ -26,6 +42,14 @@ export const default_v5_enabled = false; // 1.25
 export const default_v6_enabled = false; // 1.5
 export const default_v7_enabled = true; // 2
 export const default_v8_enabled = false; // 1.75
+export const default_w1_enabled = false; // 3.0
+export const default_w2_enabled = false; // 4.0
+export const default_w3_enabled = false; // 5.0
+export const default_w4_enabled = false; // 6.0
+export const default_w5_enabled = false; // 8.0
+export const default_w6_enabled = false; // 10.0
+export const default_w7_enabled = false; // 12.0
+export const default_w8_enabled = false; // 16.0
 
 export function value(value, defaultValue) {
     return value === undefined ? defaultValue : value;
--- inject.js
+++ inject.js
@@ -19,24 +19,29 @@
         update_class('_tap_rate_active', '_tap_rate_button_' + value.toString().replace('.', '_'), '_tap_rate_active');
     }
 
-    function onPlaybackRateChange(e) {
-        activate(e);
-    }
-
     const app = document.querySelector('ytd-app') ?? document.body; // YouTube.com or Embedded Player
 
     let player;
+    let video;
     let area;
 
     document.addEventListener('_tap_rate_loaded', () => {
         if (player) {
-            activate(player.getPlaybackRate());
+            if (video) {
+                activate(video.playbackRate);
+            } else {
+                activate(player.getPlaybackRate());
+            }
         }
     });
 
     document.addEventListener('_tap_rate', e => {
         if (player) {
             player.setPlaybackRate(e.detail);
+            if (video) {
+                video.playbackRate = e.detail;
+            }
+            activate(e.detail);
             setTimeout(() => player.dispatchEvent(new MouseEvent('mouseout')), 500);
         }
     });
@@ -47,14 +52,17 @@
             return;
         }
 
+        video = app.querySelector('video.html5-main-video');
+        if (!video) {
+            return;
+        }
+
         area = player.querySelector('div.ytp-right-controls');
         if (!area) {
             return;
         }
 
         clearInterval(detect_interval);
-
-        player.addEventListener('onPlaybackRateChange', onPlaybackRateChange);
 
         document.dispatchEvent(new CustomEvent('_tap_rate_init'));
     }, 500);
